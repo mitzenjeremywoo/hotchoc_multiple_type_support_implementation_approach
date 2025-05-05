4e09a878badd18f366687a3b9687935cb18ef18d
jeremy woo <jeremy>
2025-05-06 04:32:12 +1200

    update code

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 using apollo_gateway.Types;
+using apollo_gateway.Types.CodeFirst;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// code first approach - letting you customize graphql
-//builder.Services.AddGraphQLServer().AddApolloFederation().AddQueryType<BookCodeFirstApproachQueryType>();
+//code first approach - letting you customize graphql
+builder.Services.AddGraphQLServer().AddApolloFederation().AddQueryType<BlogQueryType>();
 
 // implementation approach
-builder.Services.AddGraphQLServer().AddApolloFederation().AddQueryType(q => q.Name("Query"))
-    .AddType<BookDataReturnDataHelper>().AddType<ProductDataReturnDataHelper>();
+//builder.Services.AddGraphQLServer().AddApolloFederation().AddQueryType(q => q.Name("Query"))
+//    .AddType<BookDataReturnDataHelper>().AddType<ProductDataReturnDataHelper>();
 
 var app = builder.Build();
 
